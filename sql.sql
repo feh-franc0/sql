513a4900162ada4cad344aa35e91fcdb2b18871c
feh-franc0 <fernandofv1110@gmail.com>
2022-01-10 20:51:43 -0300

    O que é sql

---
+++ sql.sql
@@ -0,0 +1,3 @@
+/*
+Structured Query Language, ou Linguagem de Consulta Estruturada ou SQL, é a linguagem de pesquisa declarativa padrão para banco de dados relacional. Muitas das características originais do SQL foram inspiradas na álgebra relacional.
+*/
