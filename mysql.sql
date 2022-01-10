6a0bc6fe6bf9457e0537f813fc0c9ac2e6756076
feh-franc0 <fernandofv1110@gmail.com>
2022-01-10 20:43:49 -0300

    O que é MySql

---
+++ mysql.sql
@@ -0,0 +1,3 @@
+/*
+O MySQL é um sistema gerenciador de banco de dados relacional de código aberto usado na maioria das aplicações gratuitas para gerir suas bases de dados. ... Na criação de aplicações web abertas e gratuitas, o conjunto de aplicações mais usado é o LAMP, um acrônimo para Linux, Apache, MySQL e Perl/PHP/Python.
+*/
